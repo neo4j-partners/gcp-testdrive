1df3c3d30d9d6fdeeb7c54aca010662191005be6
Moxious <moxious@oldhat.org>
2018-01-17 12:34:56 -0500

    updates

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Files related to GCP test-drive for neo4j-enterprise
 
 Google internal docs for how to create testdrives:
 https://cloud.google.com/launcher/docs/partners/test-drive-single-vm
+
+Files in this repo store the content of the test drive (the browser autoplay
+guide) and notes about how the VM was set up and configured.
+
+Two GCP projects are relevant for this work:
+
+```
+PROJECT_ID                   NAME                    PROJECT_NUMBER
+test-drive-development       test-drive-development  751718636122
+test-drive-public            test-drive-public       533290682560
+```
--- vmsetup.sh
+++ vmsetup.sh
@@ -9,28 +9,6 @@ sudo apt-get install dirmngr --install-recommends
 sudo apt-get update
 sudo apt-get install software-properties-common
 
-# Key for the java repo we're about to add...
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-
-sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
-
-sudo apt-get update
-
-# Install java 8, accept licenses
-sudo apt-get install oracle-java8-installer
-
-# If download step fails, download manually and place installer
-# in /var/cache/oracle-jdk8-installer and re-run install of that
-# installer package
-#
-# If this still doesn't work (because it can get weird on google images)
-# then manually install
-# https://www.digitalocean.com/community/tutorials/how-to-manually-install-oracle-java-on-a-debian-or-ubuntu-vps
-
-# sudo update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_161/bin/java 100 
-# sudo update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_161/bin/javac 100
-
-
 ####
 ### Neo4j Portion
 ####
